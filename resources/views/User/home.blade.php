6b31870e7a936f3d3f30c46dcae88e96f4791034
najibghani <najibghani80@gmail.com>
2024-05-26 19:45:53 +0700

    Deskripsikan perubahan Anda di sini

---
+++ resources/views/User/home.blade.php
@@ -0,0 +1,509 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
+    <style>
+      body {
+    background-color: #e7dbdb
+
+  };
+
+  .navbar{
+    position: fixed;
+  }
+
+  span{
+    color:rgb(165, 165, 165);
+  }
+
+  .custom {
+    background-color: black;
+    color: white;
+    width: 337px;
+    border-radius: 20px;
+  }
+
+  .opening{
+    display: flex;
+  }
+  .pembuka{
+    margin-top: 15%;
+    margin-right: 453px;
+    margin-left: 188px;
+    font-size: x-large;
+  }
+
+  .batik-atas{
+    margin-top:75px;
+  }
+
+  .getstarted{
+    background-color: black;
+    color: white;
+    font-size: large;
+    margin-bottom: 2%;
+    border-radius: 20px;
+    height:33px;
+    padding: 0px 40px 0px 40px;
+  }
+
+  .turun {
+    margin-left: 7rem;
+    margin-top: 2rem;
+  }
+
+  .batik-atas{
+    border:20px;
+    display: flex;
+  }
+
+  .pict1 img{
+    margin-right:15px;
+    width:160px;
+    height:355px;
+  }
+
+  .pict2 img{
+
+    width:160px;
+    height:355px;
+    margin-right:15px;
+   
+  }
+
+  .pict3 img{
+    width:160px;
+    height:355px;
+  }
+
+  .collection{
+    text-align: center;
+    margin-top: 10rem;
+  }
+
+  .deskripsi{
+    font-size: medium;
+  }
+  
+  .foto1{
+    margin:auto ;
+
+  }
+  #img1{
+    display: flex;
+    
+  }
+
+  #img1 img{       
+    position:relative;
+    height: 337px;
+    width:  264px;
+    margin-top: 75px;
+   
+  }
+  
+ 
+  .batik{
+    text-align: center;
+    font-family: 'Times New Roman', Times, serif;
+    margin-left: 13cm;
+   
+  }
+  
+  .kebaya{     
+    text-align: center;
+    font-family: 'Times New Roman', Times, serif;
+    margin-left: 1cm;
+  }
+
+  .berita{
+    display:flex;
+    margin:auto;
+    background-color: #D2CECE;
+    width:1138px;
+    height:310px;
+    border: 4px solid #9F959E;
+    border-radius:15px;
+    margin-top:155px;
+    
+  }
+
+  .mas img{
+    width: 178px;
+    height:256px;
+    margin-right:75px;
+    margin-top:20px;
+  }
+
+  .mba img{
+    width: 256px;
+    height:256px;
+    margin-top:20px;
+  }
+
+  .diskon{
+    text-align:center;
+    width: 747px;
+    height:76px;
+    margin-top:75px;
+  }
+
+  .bestsell{
+    text-align: center;
+    margin-top: 175px;
+  }
+
+  .baru{
+    border: 2px solid black; 
+    padding: 20px; 
+    width: 200px; 
+    height: 300px;
+    margin-left:155px ;
+  }
+
+ 
+  
+
+  .outer-frame {
+    border: 2px solid #333; 
+    border-radius: 9px;
+    padding: 20px; 
+    width: 308px;
+    height: 427px;
+    margin-left:150px;
+    margin-right: 85px;
+    margin-bottom:75px;
+  }
+  
+
+  .inner-frame {
+    border: 1px solid #666; 
+    border-radius: 45px;
+    padding: 20px;
+    width: 252px;
+    height: 233px;
+    margin-left: 5px;
+  }
+
+
+  .add{
+    background-color: #A01B5A;
+    width: 202px;
+    height:30px;
+    border-radius: 20px;
+    color: white;
+  }
+  
+  .radio{
+        text-align:center;
+      }
+
+  .about{
+    margin:auto;
+    background-color: #D2CECE;
+    width:1130px;
+    height:410px;
+    border: 4px solid #9F959E;
+    border-radius:15px;
+    margin-top:75px;
+    text-align:center;
+    padding-top:113px;
+  }
+
+  .lokasi{
+    margin-top:95px;
+    text-align:center;
+  }
+
+  
+
+  @media (max-width:576px){}
+      
+      
+      .
+      .radio{
+        text-align:center;
+      }
+
+      .about{
+        font-size:small;
+      }
+  
+    </style>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+  
+    <div class="col-3">
+      <h1><span><b>S</b></span>AERAH</h1>
+    </div>
+  
+    <div class="navbar">
+    <a class="navbar-brand" href="#">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Product</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-3" href="#tentang">About Us</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+                        <input class="form-control me-2 custom" type="search" placeholder="Cari Disini" aria-label="Search">
+                        <button class="btn btn-outline-secondary bi bi-search cuzz" type="submit"></button>
+        </form> 
+        <a href="#"><i class="bi bi-person-circle me-3">Masuk</i></a>
+        <a href="#"><i class="bi bi-cart3 me-3"></i></a>
+        
+      </div>
+    </div>
+  </nav>
+   
+  <br>
+
+<div class="opening">
+  <div class="pembuka">
+   <h2>
+    Mari Jelajahi Koleksi <br>
+    Fashion Kami yang<br>
+    Eksklusif. <button class="getstarted" id="scrollButton" 
+    onclick=""> <i class="bi bi-caret-right"></i>Get Started</button><br>
+    <div class="turun"><i class="bi bi-chevron-down"></i></div>    
+  </h2>
+</div>
+
+<div class="batik-atas">
+  <div class="pict1">
+   <img src="assets/batikpanjang1.jpg" >
+  </div>
+
+  <div class="pict2">
+    <img src="assets/batikpanjang2.jpg" >
+  </div>
+
+  <div class="pict3">
+    <img src="assets/batikpanjang3.jpg" >
+  </div>
+</div>
+</div>
+
+
+ <div class="collection">
+  <h3>Saerah Collection</h3>
+  <h1 class="deskripsi">Di Toko Saerah menyediakan berbagai macam baju<br>Batik, Kebaya</h1>
+ </div>
+
+ <div class="foto1">
+  <div id="img1">
+   <div class="batik">
+    <img src="assets/modelcowok.png" >
+    <h1 style="font-size: x-large;"> Batik</h1>
+   </div>
+   <br>
+   <div class="kebaya">
+    <img src="assets/modelcewek.png">
+    <h1 style="font-size: x-large;">Kebaya</h1>
+   </div>
+ </div>
+</div> 
+
+<div class="berita">
+  <div class="mas">
+  <img src="assets/mas.png" >
+  </div>
+  <div class="diskon">
+  <h2 class=" style="font-size:xx-large">S A E R A H | S P E S I A L  E D I S I</h2><br>
+  <h1 style="font-size:large;">Diskon Besar Besaran Hingga 80%, Nikmati<br> Berbelanja dengan Senang</h1>
+  </div>
+  <div class="mba">
+  <img src="assets/mba.png">
+  </div>
+</div>
+
+<div class="bestsell">
+  <h3>Saerah Best Seller</h3>
+</div>
+
+
+<div id="carouselExampleIndicators" class="carousel slide">
+   <div class="carousel-indicators">
+     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+   </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active d-inline-flex">
+          <div class="slide-first">
+      <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Batik</h2>
+        <h1 style="font-size: larger;">Kemeja kekinian<br>dikombinasi dengan batik</h1>
+        <div class="tambah">
+          <button class="add"  onclick="">
+          <i class="bi bi-cart3 me-3"></i> <a href="#"></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Kebaya</h2>
+        <h1 style="font-size: larger;">Kebaya dengan berbagai<br>model dan warna</h1>
+        <div class="tambah">
+          <button class="add" id="scrollButton" onclick="">
+            <a href="#"><i class="bi bi-cart3 me-3"></i></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Kain Batik</h2>
+        <h1 style="font-size: larger;">Kain batik tradisional</h1>
+        <div class="tambah">
+          <button class="add" id="scrollButton" onclick="">
+            <a href="#"><i class="bi bi-cart3 me-3"></i></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+    </div>
+          </div>
+
+          <div class="carousel-item position-fixed">
+          <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Batik</h2>
+        <h1 style="font-size: larger;">Kemeja kekinian<br>dikombinasi dengan batik</h1>
+        <div class="tambah">
+          <button class="add"  onclick="">
+          <i class="bi bi-cart3 me-3"></i> <a href="#"></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Kebaya</h2>
+        <h1 style="font-size: larger;">Kebaya dengan berbagai<br>model dan warna</h1>
+        <div class="tambah">
+          <button class="add" id="scrollButton" onclick="">
+            <a href="#"><i class="bi bi-cart3 me-3"></i></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Kain Batik</h2>
+        <h1 style="font-size: larger;">Kain batik tradisional</h1>
+        <div class="tambah">
+          <button class="add" id="scrollButton" onclick="">
+            <a href="#"><i class="bi bi-cart3 me-3"></i></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+    </div>
+          </div>
+
+          <div class="carousel-item position-fixed">
+          <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Kebaya</h2>
+        <h1 style="font-size: larger;">Kebaya dengan berbagai<br>model dan warna</h1>
+        <div class="tambah">
+          <button class="add" id="scrollButton" onclick="">
+            <a href="#"><i class="bi bi-cart3 me-3"></i></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <div class="outer-frame">
+        <div class="inner-frame">
+          <img src="" alt="">
+        </div>
+        <h2>Kain Batik</h2>
+        <h1 style="font-size: larger;">Kain batik tradisional</h1>
+        <div class="tambah">
+          <button class="add" id="scrollButton" onclick="">
+            <a href="#"><i class="bi bi-cart3 me-3"></i></a>Add
+          </button>
+          <a href="#"><i class="bi bi-heart me-3"></i></a>
+        </div>
+    </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+  
+
+</div>
+
+<section id="tentang">
+<div class="about">
+  <h3>About Us</h3>
+  <br>
+  <h1 style="font-size:large;">Selamat datang di Saerah, rumah bagi keindahan tradisi dalam busana.<br>
+  Kami dengan bangga menghadirkan koleksi baju adat yang indah dan beragam, merangkul kekayaan budaya Indonesia.<br>
+  Dalam setiap jahitan dan motif, kami memadukan keterampilan tradisional dengan desain yang menarik,<br>
+  menciptakan busana adat yang tak hanya mempesona tapi juga relevan dalam gaya masa kini.</h1>
+</div>
+</section>
+
+<div class="lokasi">
+<h3>Lokasi</h3>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.120062121915!2d110.47570119999996!3d-6.995138499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708d1fb98592af%3A0x870265347dce57e3!2sgraha%20rias%20KRATON!5e0!3m2!1sid!2sid!4v1715910399923!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        width="1100"
+        height="400"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/Home', 'index')->name('index');
 });
